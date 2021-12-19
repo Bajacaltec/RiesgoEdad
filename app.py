84ea080bab880a6a302e00ea69530f129ffd3d29
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-12-19 14:18:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from streamlit.proto.RootContainer_pb2 import SIDEBAR
 st.image("medpost.jpeg")
 st.sidebar.subheader("Coloca tu edad en la barra y se desplegarán los riesgos a la salud más importanes, así como recomendaciones actualizadas según las guias internacionales y de México")
 
-col1,col2 =st.beta_columns(2)
+col1,col2 =st.columns(2)
 with col1: 
     ""
 with col2:
