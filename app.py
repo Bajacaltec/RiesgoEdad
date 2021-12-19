03fae948377e61ad16d9e3090bc2ba6276315b6b
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-12-19 14:17:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from streamlit.proto.RootContainer_pb2 import SIDEBAR
 #APP con una regla de edad (linea) que con el aumento de la edad se pueda desplegar los
 #riesgos mas importantes y como prevenirlos
 st.image("medpost.jpeg")
+    st.sidebar.subheader("Coloca tu edad en la barra y se desplegarán los riesgos a la salud más importanes, así como recomendaciones actualizadas según las guias internacionales y de México")
 
-st.sidebar.subheader("Coloca tu edad en la barra y se desplegarán los riesgos a la salud más importanes, así como recomendaciones actualizadas según las guias internacionales y de México")
-edad=st.slider('Edad',1,100,value=None,step=1)
-if edad==32:
+col1,col2 =st.beta_columns(2)
+with col1: 
+    ""
+with col2:
+    edad=st.slider('Edad',1,100,value=None,step=1)
+    if edad==32:
     st.balloons()
