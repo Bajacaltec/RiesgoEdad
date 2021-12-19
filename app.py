f4f9f7bbbed9d0057979c21349217952590f4662
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-12-19 14:01:17 -0600

    Riesgo salud

---
+++ app.py
@@ -0,0 +1,9 @@
+from altair.vegalite.v4.api import value
+import streamlit as st
+from streamlit.proto.RootContainer_pb2 import SIDEBAR
+#APP con una regla de edad (linea) que con el aumento de la edad se pueda desplegar los
+#riesgos mas importantes y como prevenirlos
+st.image("medpost.jpeg")
+
+st.sidebar.subheader("Coloca tu edad en la barra y se desplegarán los riesgos a la salud más importanes, así como recomendaciones actualizadas según las guias internacionales y de México")
+st.slider('Edad',1,100,value=None,step=1)
